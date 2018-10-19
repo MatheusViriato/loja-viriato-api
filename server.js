61723f42822ca1ed36d07d7cd9806156a983ffd7
MatheusViriato <matheus.viriato@hotmail.com>
2018-10-19 12:40:31 -0300

    editing server.js

--- server.js
+++ server.js
@@ -9,10 +9,38 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host : 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user : 'lql9yqvnqldeclst',
+    password : 't36fg32wuxbhe447',
+    database : 'bun62q03wxr00su2',
+    port: '3306'
+});
+
 app.listen(port);
 
 console.log("Servidor HTTP está na porta " + port);
 
-app.get('/', function(req, res){
-    res.send({msg:'Olá'});
+app.get('/api', function(req, res){
+    var data = req.body;
+    var response = [];
+
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "select * from products where category = " + data.category;
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            for(var i = 0; i < result.length; i++){
+                response.push({
+                    img: result[i].img, 
+                    name: result[i].name,
+                    description: result[i].description,
+                    price: result[i].price
+                })
+            }
+            res.send(response);
+        });
+    });
 });
