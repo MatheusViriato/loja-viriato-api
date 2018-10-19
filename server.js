d4f90996277d980b5f465dc60c9025487027e365
MatheusViriato <matheus.viriato@hotmail.com>
2018-10-19 11:00:31 -0300

    api

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//body-parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var port = 8080;
+
+app.listen(port);
+
+console.log("Servidor HTTP está na porta " + port);
+
+app.get('/', function(req, res){
+    res.send({msg:'Olá'});
+});
