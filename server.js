7d633f8536e5e9098994419a1718eb2ff96335a1
MatheusViriato <matheus.viriato@hotmail.com>
2018-10-19 15:00:45 -0300

    config server

--- server.js
+++ server.js
@@ -24,12 +24,37 @@ app.listen(port);
 console.log("Servidor HTTP está na porta " + port);
 
 app.get('/api', function(req, res){
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
     var data = req.body;
     var response = [];
 
     con.connect(function(err) {
         if (err) throw err;
         var sql = "select * from products";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            for(var i = 0; i < result.length; i++){
+                response.push({
+                    img: result[i].img, 
+                    name: result[i].name,
+                    description: result[i].description,
+                    price: result[i].price
+                })
+            }
+            res.send(response);
+        });
+    });
+});
+
+app.post('/api/category', function(req, res){
+    var data = req.body;
+    var response = [];
+
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "select * from products where category = " + data.category;
         con.query(sql, function (err, result) {
             if (err) throw err;
             for(var i = 0; i < result.length; i++){
