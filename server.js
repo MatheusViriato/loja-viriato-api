3279fe35441f993c444ff62bdab9b4bea42e17d8
Matheus Viriato <matheus.nascimento@abril.com.br>
2018-10-21 00:47:08 -0300

    edit server

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
-
 var mysql = require('mysql');
-
 var con = mysql.createConnection({
     host : 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user : 'lql9yqvnqldeclst',
@@ -20,24 +17,22 @@ var con = mysql.createConnection({
 
 app.listen(port);
 
-console.log("Servidor HTTP está na porta " + port);
-
 app.get('/api', function(req, res){
-
     res.setHeader("Access-Control-Allow-Origin", "*");
-
-    var data = req.body;
     var response = [];
-
-    var sql = "select * from products";
+    var sql = "select * from produtos";
     con.query(sql, function (err, result) {
         if (err) throw err;
         for(var i = 0; i < result.length; i++){
             response.push({
                 img: result[i].img, 
-                name: result[i].name,
-                description: result[i].description,
-                price: result[i].price
+                img1: result[i].img1, 
+                img2: result[i].img2, 
+                img3: result[i].img3, 
+                name: result[i].nome,
+                description: result[i].descricao,
+                price: result[i].preco,
+                old_price: result[i].preco_antigo
             })
         }
         res.send(response);
@@ -45,21 +40,21 @@ app.get('/api', function(req, res){
 });
 
 app.get('/api/category', function(req, res){
-
     res.setHeader("Access-Control-Allow-Origin", "*");
-
-    var data = req.body;
     var response = [];
-
-    var sql = "select * from products where category = " + "'" + req.query.category + "'";
+    var sql = "select * from produtos where categoria like " + "'%" + req.query.category + "%'";
     con.query(sql, function (err, result) {
         if (err) throw err;
         for(var i = 0; i < result.length; i++){
             response.push({
                 img: result[i].img, 
-                name: result[i].name,
-                description: result[i].description,
-                price: result[i].price
+                img1: result[i].img1, 
+                img2: result[i].img2, 
+                img3: result[i].img3, 
+                name: result[i].nome,
+                description: result[i].descricao,
+                price: result[i].preco,
+                old_price: result[i].preco_antigo
             })
         }
         res.send(response);
